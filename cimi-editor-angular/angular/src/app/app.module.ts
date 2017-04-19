3d6762d200247179ac791a34a43a32b5180b75a2
Claude Nanjo <cnanjo@gmail.com>
2017-04-19 14:08:14 -0700

    Some updates to angular

--- cimi-editor-angular/angular/src/app/app.module.ts
+++ cimi-editor-angular/angular/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppComponent1} from "./app1.component";
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponent1
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent, AppComponent1 ]
 })
 export class AppModule { }
